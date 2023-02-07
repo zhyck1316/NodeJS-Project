709108180eb325250613cb73c76dcd05e46d5901
zhyck1316 <zhyck1316@gmail.com>
2023-02-08 07:19:39 +0800

    2nd Commit

--- app.js
+++ app.js
@@ -9,25 +9,25 @@ app.set('views', 'html_pages');
 app.listen(3000); 
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {title: 'Home'});
     //  res.send('<p>Homepage</p>');
 
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about',{title: 'about'});
     // res.send('<p>Homepage</p>');
 
 });
 
 app.get('/html_pages/create', (req,res) => {
-    res.render('create');
+    res.render('create',{title: 'New blog'});
 })
 // app.get('/about-us', (req, res) => {
 //     res.redirect('/about');
 // })
 
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404',{title: '404'});
 })
 
